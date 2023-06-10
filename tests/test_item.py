ecf4d4216233cef535684cc9470b89b2a301dec4
Aptemond <sofartem5@gmail.com>
2023-06-10 13:46:45 +0500

    realize Item and tests for him

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+def test_init():
+    fish = Item('fish', 100, 5)
+    assert fish.name == 'fish'
+    assert fish.price == 100
+    assert fish.quantity == 5
+
+def test_calculate_total_price():
+    Item.all = []  # Сброс списка всех товаров перед каждым тестом
+    fish1 = Item('fish1', 100, 5)
+    fish2 = Item('fish2', 200, 3)
+    assert fish1.calculate_total_price() == 1100
+
+def test_apply_discount():
+    Item.pay_rate = 0.9
+    fish = Item('fish', 100, 5)
+    fish.apply_discount()
+    assert fish.price == 90  # 100 * 0.9
